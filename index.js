7977f1dd9c8342df6ed19bd8ac3ebae9dc8c03ef
Allagappan Muthuraman <allagappan@gmail.com>
2020-03-27 10:22:20 +0530

    Add prediction

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ $(document).ready(function(){
   let total = 0;
   $.get("https://api.covid19india.org/data.json", function(response){
     let history = response.cases_time_series;
-    console.log(history);
     history.forEach(function(data, index) {
       if (index == 0) { 
         total = total + parseInt(data['dailyconfirmed'])
@@ -33,25 +32,28 @@ $(document).ready(function(){
     rate[last]['current'] = parseInt(response['statewise'][0]['confirmed']);
     rate[last]['past'] = rate[last-1]['current']
     rate[last]['rate'] = (100*(rate[last]['current'] - rate[last]['past'])/(rate[last]['past'])).toFixed(2);
-    
-    console.log(rate);
+  
     
     $("#today-growth").html(rate[last]['rate'])
     $("#today-cases").html(rate[last]['current'])
-    $("#yesterday-growth").html(rate[last-1]['rate'])
+    $("#today-new").html(rate[last]['current']-rate[last]['past'])
+    $(".yesterday-growth").html(rate[last-1]['rate'])
     $("#yesterday-cases").html(rate[last-1]['current'])
+    var yesterday_rate_prediction = Math.round(rate[last]['current']*Math.pow((100+parseFloat(rate[last-1]['rate']))/100,7))
+    $("#yesterday-rate-prediction").html(yesterday_rate_prediction)
     
     var rate_7_day_sum = 0.0;
     for (var i = rate.length - 2; i >= rate.length - 8; i--) {
       rate_7_day_sum = rate_7_day_sum + parseFloat(rate[i]['rate']);
-      console.log("i=" + i)
-      console.log("rate = " + rate[i]['rate']);
-      console.log("sum = " + rate_7_day_sum)
     }
     
     let rate_7_day_avg = (rate_7_day_sum/7)
+    var seven_day_prediction = rate[last]['current']*(Math.pow(rate_7_day_avg,7))
+    var seven_day_prediction = Math.round(rate[last]['current']*Math.pow((100+parseFloat(rate_7_day_avg))/100,7))
+    $("#7-day-prediction").html(seven_day_prediction)
     
-    $("#7-day-avg").html(rate_7_day_avg.toFixed(2));
+    $(".7-day-avg").html(rate_7_day_avg.toFixed(2));
+    
     
     var ctx = $('#growth-trends');
     var labels = rate.map(function(x) { return x['date']}).slice(last - 21, last+1);
